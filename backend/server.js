c3c7acb67da897602f236a755f15e8c4e7409d5f
NearRyuzaki122420 <renanielpardo@gmail.com>
2026-03-11 14:02:04 +0800

    Prepare app for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/sales', salesRoutes);
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
