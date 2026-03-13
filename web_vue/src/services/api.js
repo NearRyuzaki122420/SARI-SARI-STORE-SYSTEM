c19106a9aacc500454e8ae92b90501a10394dccc
NearRyuzaki122420 <renanielpardo@gmail.com>
2026-03-13 10:27:33 +0800

    Change basURL FINAL to Render

--- web_vue/src/services/api.js
+++ web_vue/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://https://sari-sari-store-system.onrender.com/api'
+  baseURL: 'https://sari-sari-store-system.onrender.com/api'
 })
 
 api.interceptors.request.use((config) => {
